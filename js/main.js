d7face11a53639037048d9890bff81f562663c26
jestrellaux <101490521+jestrellaux@users.noreply.github.com>
2022-08-07 14:35:18 -0400

    235

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ function myFunction() {
     alert("Thank you for downloading my resume");
     console.log("Hello world!");}
   
+function imageShow(){
+    document.getElementById('image').style.display = "block";       
+    }
